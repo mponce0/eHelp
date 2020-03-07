4cadd3c3b1b00c7709575357166dd4fc9193549e
Marcelo Ponce <mponce@scinet.utoronto.ca>
2020-03-07 14:40:49 -0500

    adding capability of specifying pckg base dir or dir with just R scripts

--- R/emulatePkg.R
+++ R/emulatePkg.R
@@ -1,11 +1,14 @@
 simulatePackage <- function(pkgLocation=NULL) {
 #' function that allows to load the functions from a package in preparation for
-#' CRAN, as if it is being loaded  by loading all the fns defined in R
-#' sub-directory of the package
+#' CRAN, as if it is being loaded by loading all the fns defined in the R
+#' sub-directory of the package, ie. "myPckg/R"
 #'
 #' @param  pkgLocation  path to the base loaction of the package, under which
 #' is expected to found the R sub-directory 
-
+#'
+#' @examples
+#' simulatePackage("myPckg")
+#'
 
 	checkPkgLocation <- function(pkgLoc) {
 
@@ -15,23 +18,31 @@ simulatePackage <- function(pkgLocation=NULL) {
 		if (!dir.exists(pkgLoc)) {
 			stop(pkgLoc," is not a valid directory! Please indicate the 'base' location of your package")
 		} else if (!dir.exists(R.pckg.loc)) {
-			stop("There isn't an R subdirectory found in the indicated location: ",pkgLoc)
+			warning("There isn't an R subdirectory found in the indicated location: ",pkgLoc, " -- will asumme ", pkgLoc, " to search for R sources")
+			R.pckg.loc <- pkgLoc
 		}
 
 		return(R.pckg.loc)
 	}
+
 
 	# if a package location is not specified will assume current directory
 	if (is.null(pkgLocation)) {
 		pkgLocation <- getwd()
 	}
 
+	# first try to see in there is an "R" subir within the Pckg location
 	R.pckg.loc <- checkPkgLocation(pkgLocation)
+
+	# return to original location
 	previous.loc <- getwd()
+	on.exit(setwd(previous.loc))
 
 	# cd into package location
 	setwd(R.pckg.loc)
-	pkg.files <- dir()
+
+	#pkg.files <- dir()
+	pkg.files <- list.files(pattern = "\\.r$",ignore.case=T)
 
 	# fns before loading package...
 	before <- names(as.list(.GlobalEnv))
@@ -43,7 +54,7 @@ simulatePackage <- function(pkgLocation=NULL) {
 			source(pkg.file)
 		}
 	} else {
-		stop("There were no files found in ",R.pckg.loc)
+		stop("There weren't any *R* files found in ",R.pckg.loc)
 	}
 
 	# fns after loading package...
@@ -51,6 +62,7 @@ simulatePackage <- function(pkgLocation=NULL) {
 
 	# new fns loaded...
 	if (sum(! after %in% before) > 0) {
+		cat(paste(rep('-',80),collapse=""),'\n')
 		cat("Functions and objects  loaded...",'\n')
 		#for (obj in as.list(.GlobalEnv)) {
 		#for (obj in ls()) {
@@ -59,8 +71,7 @@ simulatePackage <- function(pkgLocation=NULL) {
 		#	}
 		#}
 		print(after[!(after %in% before)])
+		cat(paste(rep('-',80),collapse=""),'\n') 
 	}
 
-	# return to original location
-	setwd(previous.loc)
 }
